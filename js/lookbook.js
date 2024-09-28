354ee043ce7eac22531fee3629b6dbff1db4196c
OBRETAS15 <juniorbretasaraujo305@gmail.com>
2024-09-27 21:54:40 -0300

    lookbook teste

---
+++ js/lookbook.js
@@ -0,0 +1,17 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.book-image');
+const totalImages = images.length;
+
+// Função para exibir a próxima imagem
+function nextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % totalImages;
+    images[currentIndex].classList.add('active');
+}
+
+// Função para exibir a imagem anterior
+function prevImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    images[currentIndex].classList.add('active');
+}
